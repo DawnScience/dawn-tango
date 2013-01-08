1fedf9d807879acb6db8771ce2261782fb8995d8
matthew.gerring <matthew.gerring@gmail.com>
2013-01-08 16:34:06 +0000

    Moved non-shared memory specific code away.

--- org.dawb.tango.extensions/src/org/dawb/tango/extensions/editors/SharedMemoryUtils.java
+++ org.dawb.tango.extensions/src/org/dawb/tango/extensions/editors/SharedMemoryUtils.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.dawb.common.ui.plot.PlotType;
+import org.dawb.common.ui.util.CalibrationUtils;
 import org.dawb.common.util.ExpressionFactory;
 import org.dawb.common.util.IExpressionEvaluator;
 import org.dawb.tango.extensions.Activator;
@@ -149,63 +150,9 @@ public class SharedMemoryUtils {
 			return AbstractDataset.arange(0, set.getSize());
 		}
 		
-        return getCalibrated(set, null, true);
+        return CalibrationUtils.getCalibrated(set, null, true);
 	}
 
-	/**
-	 * Returns the calibrated abstract data set if required, otherwise returns set
-	 * unchanged.
-	 * 
-	 * @param set
-	 * @param scalar optionally specify some extra scalar values for the calibration expression.
-	 * @return
-	 * @throws Exception
-	 */
-	public static IDataset getCalibrated(final IDataset set, final Map<String,String> scalar, final boolean checkEnabled) throws Exception {
-		
-		
-		final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-		
-		if (checkEnabled) {
-			final boolean        isCalib = store.getBoolean(CalibrationConstants.USE);
-			if (!isCalib)  return AbstractDataset.arange(0, set.getSize(), 1, AbstractDataset.INT32);
-		}
-		
-     	final String expr = store.getString(CalibrationConstants.EXPR);
-		final String name = store.getString(CalibrationConstants.LABEL);
-		final double a    = store.getDouble(CalibrationConstants.A);
-		final double b    = store.getDouble(CalibrationConstants.B);
-		final double c    = store.getDouble(CalibrationConstants.C);
-		final double d    = store.getDouble(CalibrationConstants.D);
-		
-		
-		final double[] calib = new double[set.getSize()];
-		
-		// TODO FIXME - What is the definition of p and p0??
-		final double p0 = 0;
-	
-		final IExpressionEvaluator eval = ExpressionFactory.createExpressionEvaluator();
-		eval.setExpression(expr);
-		
-		final Map<String,Object> vals = new HashMap<String,Object>(7);
-		for (int i = 0; i < calib.length; i++) {
-			
-			vals.clear();
-			if (scalar!=null) vals.putAll(scalar);
-			vals.put("p",  i);
-			vals.put("p0", p0);
-			vals.put("a",  a);
-			vals.put("b",  b);
-			vals.put("c",  c);
-			vals.put("d",  d);
-			
-			calib[i] = eval.evaluate(vals);
-		}
-		
-		DoubleDataset ret = new DoubleDataset(calib, calib.length);
-		ret.setName(name);
-		return ret;	
-	}
 
 	/**
 	 * true if calibration is used.
